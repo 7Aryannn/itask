9f88dadb22287ea603bcb78e255727bc1d7d8625
7Aryannn <aryanshri1912@gmail.com>
2026-03-01 06:13:08 +0530

    ui: centered navbar logo and matched color to background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,3 @@
-// import React from 'react'
-
-// const Navbar = () => {
-//     return (
-//         <nav className='flex justify-center bg-[#0a0a0f] text-white py-4 border-b border-white/[0.06]'>
-//             <span className='font-extrabold text-3xl tracking-widest bg-gradient-to-br from-white via-white to-violet-400 bg-clip-text text-transparent'>iTask</span>
-//         </nav>
-//     )
-// }
-
-// export default Navbar
-
 import React from 'react'
 
 const Navbar = () => {
